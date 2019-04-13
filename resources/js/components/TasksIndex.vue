84b82eefe962d2620e04e0617e617fdbb8ab87c6
Alexander Nichiporenko <alex.n@hamelius.ru>
2019-04-13 15:27:19 +0300

    Api.Tasks.Index.Frontend.

--- resources/js/components/TasksIndex.vue
+++ resources/js/components/TasksIndex.vue
@@ -7,21 +7,37 @@
                 <th class="tasks-column">Name</th>
                 <th class="tasks-column">Priority</th>
             </tr>
-            <!-- <tr>
-                <th class="tasks-column">{{ $task->id }}</th>
-                <th class="tasks-column">{{ $task->name }}</th>
-                <th class="tasks-column">{{ $task->priority }}</th>
-            </tr> -->
+            <tr v-for="task in tasks" :key="task.id">
+                <th class="tasks-column">{{ task.id }}</th>
+                <th class="tasks-column">{{ task.name }}</th>
+                <th class="tasks-column">{{ task.priority }}</th>
+            </tr>
         </table>
     </div>
 </template>
 
 <script>
 export default {
-    //
+    data() {
+        return {
+            tasks: []
+        }
+    },
+    created() {
+        axios.get('/api/tasks')
+            .then((response) => this.tasks = response.data.data)
+            .catch((error) => console.log(error))
+    }
 }
 </script>
 
 <style scoped>
-
+    .tasks-index { 
+        border: 1px solid rgb(222, 222, 222); 
+        border-collapse: collapse; 
+    }
+    .tasks-column { 
+        border: 1px solid rgb(222, 222, 222); 
+        padding: 5px; 
+    }
 </style>
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,18 +1,5 @@
 @extends('layouts.app')
 
-@section('styles')
-    <style>
-        .tasks-index { 
-            border: 1px solid rgb(222, 222, 222); 
-            border-collapse: collapse; 
-        }
-        th, td { 
-            border: 1px solid rgb(222, 222, 222); 
-            padding: 5px; 
-        }
-    </style>
-@endsection
-
 @section('content')
 <div class="container">
     <tasks-index></tasks-index>
